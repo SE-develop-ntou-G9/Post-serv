cb21e4814e7a0f9cc769a8e761c2d802d477c195
unknown <liuchangyan1014@gmail.com>
2025-10-21 20:53:05 +0800

    environment built

--- API/dto/driver_post.py
+++ API/dto/driver_post.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 from typing import Optional
 
 class DriverPostDTO(BaseModel):
+    id: int
+    vehicle_info: str
     driver_id: int
     post_id: int
     timestamp: Optional[str] = None
